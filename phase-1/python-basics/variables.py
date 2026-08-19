4e445c9340e032a71e26eae25a851128b3b9d136
Arthur Mulunda <arthurmulunda941@gmail.com>
2026-08-19 22:37:54 +0300

    day 2: variables, types, and int/str conversion errors

---
+++ phase-1/python-basics/variables.py
@@ -0,0 +1,19 @@
+name = "Arthur"
+age = 3
+is_learning = True
+
+print(name)
+print(age)
+print(is_learning)
+
+age_str = "3"
+print(age_str)
+print(type(age_str))
+print(type(age))
+
+print(age + 5)
+print(age_str + "5")
+
+print(int(age_str) + 5)
+
+print(int("hello") + 5)
