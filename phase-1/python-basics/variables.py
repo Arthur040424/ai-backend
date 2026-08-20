0156e6e9cf656ad8fff5987a3f08515914b29431
Arthur Mulunda <arthurmulunda941@gmail.com>
2026-08-20 22:50:06 +0300

    day 3: if/elif/else conditionals and for loop iteration over a list

--- phase-1/python-basics/variables.py
+++ phase-1/python-basics/variables.py
@@ -1,5 +1,5 @@
 name = "Arthur"
-age = 3
+age = 15
 is_learning = True
 
 print(name)
@@ -16,4 +16,21 @@ print(age_str + "5")
 
 print(int(age_str) + 5)
 
-print(int("hello") + 5)
+# print(int("hello") + 5)
+
+if age >= 18:
+    print("You are an adult")
+# elif 13 <= age <= 17: also works the same way
+elif age >= 13 and age <= 17:
+    print("You are a teenager")
+
+
+ages = [15, 22, 8, 30, 13]
+
+for age in ages:
+    if age >= 18:
+        print(age, "is an adult")
+    elif 13 <= age <= 17:
+        print(age, "is a teenager")
+    else:
+        print(age, "is a child")
