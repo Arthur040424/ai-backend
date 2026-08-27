739b70f8b9e1fa8e873c26b7214eeb434ae123e6
Arthur Mulunda <arthurmulunda941@gmail.com>
2026-08-27 10:25:08 +0300

    day 4: functions, parameters, return vs print, modulo operator

---
+++ phase-1/python-basics/functions.py
@@ -0,0 +1,28 @@
+def classify_age(age):
+  if age >= 18:
+    print("Adult")
+  elif 13 <= age <= 17:
+    print("Teenager")
+  else:
+    print("Child")
+
+result = classify_age(20)
+# print(result)
+# result = classify_age(15)
+# print(result)
+# result = classify_age(5)
+# print(result)
+# result = classify_age(45)
+# print(result)
+
+def is_even(number):
+  if number % 2 == 0:
+    return True
+  else:
+    return False
+
+result = is_even(4)
+print(result)
+
+result = is_even(7)
+print(result)
