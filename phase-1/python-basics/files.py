eab6c60fe687ce0031f73519466987fce6d774d6
Arthur Mulunda <arthurmulunda941@gmail.com>
2026-08-29 10:01:06 +0300

    day 8: reading files, FileNotFoundError handling

--- phase-1/python-basics/files.py
+++ phase-1/python-basics/files.py
@@ -1,2 +1,9 @@
 with open("greeting.txt", "w") as f:
     f.write("Hello, this file was written by python.\n")
+
+try:
+    with open("missing.txt", "r") as f:
+        contents = f.read()
+        print(contents)
+except FileNotFoundError:
+    print("Error: The file does not exist.")
