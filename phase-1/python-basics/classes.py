fd11816dab7262de097722148deb945288f285e2
Arthur Mulunda <arthurmulunda941@gmail.com>
2026-09-03 21:49:32 +0300

    day 11: inheritance, super(), method overriding

--- phase-1/python-basics/classes.py
+++ phase-1/python-basics/classes.py
@@ -11,3 +11,14 @@ print(arthur.introduce())
 
 jane = Person("Jane", 25)
 print(jane.introduce())
+
+class Student(Person):
+        def __init__(self, name, age, school):
+            super().__init__(name, age)
+            self.school = school
+
+        def introduce(self):
+            return f"Hi, I'm {self.name},  {self.age} years old, and I study at {self.school}."
+
+arthur_student = Student("Arthur", 20, "KINAP")
+print(arthur_student.introduce())
